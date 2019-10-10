27243df3812ceeb24b6631f60f9633dadf38f52c
EngiGu <sayheya@qq.com>
2019-10-10 19:41:55 +0800

    update

--- server/celery_senders/base_sender.py
+++ server/celery_senders/base_sender.py
@@ -1,5 +1,5 @@
 import base64
-from core.logger import get_logger
+# from core.logger import get_logger
 import logging
 
 
--- server/celery_senders/celery_config.py
+++ server/celery_senders/celery_config.py
@@ -1,5 +1,6 @@
-BROKER_URL = 'redis://192.168.170.132:6379/0'               # 指定 Broker
-CELERY_RESULT_BACKEND = 'redis://192.168.170.132:6379/1'  # 指定 Backend
+
+BROKER_URL = 'redis://192.168.170.132:6379/10'               # 指定 Broker
+CELERY_RESULT_BACKEND = 'redis://192.168.170.132:6379/11'  # 指定 Backend
 CELERY_TIMEZONE='Asia/Shanghai'                     # 指定时区，默认是 UTC
 # CELERY_TIMEZONE='UTC'
 # CELERY_IMPORTS = (                                  # 指定导入的任务模块
