d176b2ce9b402821b9320e091b8709acb6fd19d7
EngiGu <sayheya@qq.com>
2019-10-10 19:14:24 +0800

    update

--- server/server.py
+++ server/server.py
@@ -1,9 +1,69 @@
-from flask import Flask
-app = Flask(__name__)
+import logging
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+import tornado.ioloop
+import tornado.web
+import tornado.log
 
-if __name__ == '__main__':
-    app.run(port=8888)
+from celery_senders.sender import send_notice
+
+tornado.log.enable_pretty_logging()
+
+class BaseRequestHandler(tornado.web.RequestHandler):
+    # def prepare(self):
+    #     try:
+    #         # self.json_args = json.loads(self.request.body)
+    #         post_data = self.request.body_arguments
+    #         self.json_args = {x: post_data.get(x)[0].decode("utf-8") for x in post_data.keys()}
+    #     except:
+    #         self.json_args = 0
+    #     # print(self.request.body_arguments)
+    pass
+
+
+#
+# def args(func):
+#     def args_wrapper(self, *args, **kwargs):
+#         print(args)
+#         print(kwargs)
+#         print(self)
+#         for k, v in kwargs:
+#             if k in self.json_args:
+#                 kwargs[v] = self.json_args[k]
+#         return func(self, *args, **kwargs)
+#
+#     return args_wrapper
+
+
+class MainHandler(BaseRequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+    # @args
+    def post(self):
+        way = self.get_body_argument('way')
+        title = self.get_body_argument('title')
+        content = self.get_body_argument('content')
+        # print(self.request.body)
+        if not (title and content):
+            raise Exception('params error')
+
+        # if way not in ALL_SENDERS:
+        #     raise Exception('send way error')
+        # try:
+        #     s = ALL_SENDERS[way]()
+        #     s.send(title, content)
+        send_notice.delay(way, title, content)
+        self.finish({'msg': 'ok!'})
+
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/notice", MainHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
