448b8cb372dddedc7097e95475c915f2462f02c6
EngiGu <sayheya@qq.com>
2019-10-11 11:50:16 +0800

    更新cli

--- cli/cli.py
+++ cli/cli.py
@@ -2,19 +2,19 @@ import requests
 
 
 class Cli:
-    def __init__(self, server_url):
-        self.server_url = server_url
-        pass
+    def __init__(self):
+        self.server_url = 'http://sooko.tk:8888/notice'
+
+    def send(self, way, title, content):
+        data = {'title': title, 'content': content, 'way': way}
+        return requests.post(url=self.server_url, data=data).content.decode()
 
     def test(self):
         # data = {'title': 1, 'content': '[(1,2), (3,6)]', 'way': 'SinaEmail'}
         data = {'title': 1, 'content': '[(1,2), (3,6)]', 'way': 'ServerChan'}
-        print(requests.post(url=server_url, data=data).content.decode())
-        # print(requests.get(url=server_url, params=data).content.decode())
-        pass
+        print(self.send(**data))
 
 
 if __name__ == '__main__':
-    server_url = 'http://127.0.0.1:8888/notice'
-    c = Cli(server_url)
+    c = Cli()
     c.test()
